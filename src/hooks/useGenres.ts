049b23c693f8ea4a9fb7894d31be3be844ed07f2
JieLuis <104972485+JieLuis@users.noreply.github.com>
2024-03-17 17:00:38 +0800

    Delete inital data in useGenres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,7 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 
 import APIClient from "../service/api-client";
 
-import genres from "../data/genre";
 import { Genre } from "../entities/Genre";
 
 const apiClient = new APIClient<Genre>("/genres");
@@ -11,7 +10,6 @@ const useGenres = () =>
     queryKey: ["geners"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000,
-    initialData: { count: genres.length, results: genres },
   });
 
 export default useGenres;
